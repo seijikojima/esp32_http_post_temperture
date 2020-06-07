1173244e7bb2fa95b649a9e5853552066d1162f9
sabosan <sabosan331@gmail.com>
2020-06-07 20:29:55 +0900

    complete post

--- esp32_http_post_temperture.ino
+++ esp32_http_post_temperture.ino
@@ -3,6 +3,10 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
+// https://forum.arduino.cc/index.php?topic=624828.0
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+
 #include <DHT.h>
 const int PIN_DHT = 4;
 DHT dht(PIN_DHT,DHT11);
@@ -11,24 +15,25 @@ DHT dht(PIN_DHT,DHT11);
 const char* ssid = "";
 const char* password = "";
 WebServer server(80);
-
 IPAddress ipadr;
 
+const char* serverName = "";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000; // 5 second
 
 void handleRoot() {
-  String style = "<style>body { background-color : gray;}";
+  String style = "<style>body { background-color : white;}";
   style += "button { font-size : 2.0em;}";
-  style += "div {border: 3px solid #aaa; margin: 1% 10% 0% 10%; padding: 3% 20% 3% 30%; }</style>";
+  style += "div {border: 3px solid #aaa; margin: 100px 10% 0% 10%; padding: 3% 20% 3% 30%; }</style>";
 
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   
   String text = "<div>";
-  text += "<h3>" + String(h) + "%" + "</h3>";
-  text += "<h3>" + String(t) + "degree" + "</h3>";
+  text += "<h3>" + String(h) + " %" + "</h3>";
+  text += "<h3>" + String(t) + " degree" + "</h3>";
   text += "</div>";
-//  text +="<a href=\"/H\"><button>pin 5 on</button></a>";
-//  text += "<a href=\"/L\"><button>pin 5 off</button></a>\n";
   text = style + text;
   server.send(200, "text/HTML", text);
 }
@@ -44,7 +49,6 @@ void setup() {
       delay(500);  // 各回500msの時間待つ
       Serial.print(".");// 待ち時間500msごとに１ドット表示　接続が完了すると抜ける
     }
-
 
     // wifi設定表示
     Serial.println("");
@@ -82,8 +86,39 @@ void print_humidity_and_temperature(){
 }
 
 void loop(void) {
+
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+
+    HTTPClient http;
+    
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverName);
+    
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    // Prepare your HTTP POST request data
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    
+    String jsonData = "{";
+    jsonData += "\"temperature\":";
+    jsonData += String(t);
+    jsonData += ",\"humidity\":";
+    jsonData += String(h);
+    jsonData += "}"; 
+    String httpRequestData = "value1=" + String(72);
+    Serial.print("httpRequestData: ");
+    Serial.println(jsonData);
+
+    http.addHeader("Content-Type", "application/json" , "Content-Length", jsonData.length());
+    
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(jsonData);
+
+    lastTime = millis();
+  }
   
-//  print_humidity_and_temperature();
   server.handleClient();
-//  delay(3000);
 }
