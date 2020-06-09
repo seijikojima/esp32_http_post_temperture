d7e370c8318a8417cdce141e8643052486aa9ff9
sabosan <sabosan331@gmail.com>
2020-06-09 23:15:36 +0900

    execute http post by time

--- esp32_http_post_temperture.ino
+++ esp32_http_post_temperture.ino
@@ -3,21 +3,22 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
-// https://forum.arduino.cc/index.php?topic=624828.0
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
+
+#include <time.h>
+#define JST 3600* 9
 
 #include <DHT.h>
 const int PIN_DHT = 4;
 DHT dht(PIN_DHT,DHT11);
 
-
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "39626743-2.4G";
+const char* password = "22242117";
 WebServer server(80);
 IPAddress ipadr;
 
-const char* serverName = "";
+const char* serverName = "http://seijikojima.com:7001/send_data";
 
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000; // 5 second
@@ -65,6 +66,11 @@ void setup() {
     Serial.println("HTTP server started");
 
     dht.begin();
+
+    // time set
+    configTime( JST, 0, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
+
+    
 }
 
 void print_humidity_and_temperature(){
@@ -85,38 +91,61 @@ void print_humidity_and_temperature(){
   }  
 }
 
+
+void send_data(){
+
+  HTTPClient http;
+    
+  // Your Domain name with URL path or IP address with path
+  http.begin(serverName);
+  
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // humidity and temperature
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  String jsonData = "{";
+  jsonData += "\"temperature\":";
+  jsonData += String(t);
+  jsonData += ",\"humidity\":";
+  jsonData += String(h);
+  jsonData += "}";
+  
+  Serial.println(jsonData);
+
+  http.addHeader("Content-Type", "application/json" , "Content-Length", jsonData.length());
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(jsonData);
+}
+
+void execute_by_time(){
+  time_t t;
+  struct tm *tm;
+  static const char *wd[7] = {"Sun","Mon","Tue","Wed","Thr","Fri","Sat"};
+
+  t = time(NULL);
+  tm = localtime(&t);
+
+  if(tm->tm_min == 0){
+  
+    //  Serial.printf(" %04d/%02d/%02d(%s) %02d:%02d:%02d\n",
+    //        tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday,
+    //        wd[tm->tm_wday],
+    //        tm->tm_hour, tm->tm_min, tm->tm_sec);
+    send_data(); // http post
+  }
+  
+  
+}
+
 void loop(void) {
 
   //Send an HTTP POST request every 10 minutes
   if ((millis() - lastTime) > timerDelay) {
-
-    HTTPClient http;
-    
-    // Your Domain name with URL path or IP address with path
-    http.begin(serverName);
-    
-    // Specify content-type header
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    // Prepare your HTTP POST request data
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    
-    String jsonData = "{";
-    jsonData += "\"temperature\":";
-    jsonData += String(t);
-    jsonData += ",\"humidity\":";
-    jsonData += String(h);
-    jsonData += "}"; 
-    String httpRequestData = "value1=" + String(72);
-    Serial.print("httpRequestData: ");
-    Serial.println(jsonData);
-
-    http.addHeader("Content-Type", "application/json" , "Content-Length", jsonData.length());
-    
-    // Send HTTP POST request
-    int httpResponseCode = http.POST(jsonData);
-
+    execute_by_time();
     lastTime = millis();
   }
   
